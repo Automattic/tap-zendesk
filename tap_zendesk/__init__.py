eb6e90beaada7f5af8c27072ca4f92bc29c6b428
joaoamaral <7281460+joaopamaral@users.noreply.github.com>
2022-12-20 18:21:20 -0300

    Debug

--- tap_zendesk/__init__.py
+++ tap_zendesk/__init__.py
@@ -45,7 +45,7 @@ request = Session.request
 def request_metrics_patch(self, method, url, **kwargs):
     with singer_metrics.http_request_timer(None):
         response = request(self, method, url, **kwargs)
-    LOGGER.info(dict(self))
+    LOGGER.info(dir(self))
     rate_throttling(response, 650)
     return response
 
